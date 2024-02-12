5051e663021dcfef6767a732f41a529605903e52
Monicaco1988 <edmin.aguilaroddershede@gmail.com>
2024-02-12 14:46:42 +0100

    Update starting to get the signal/emit stuff

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using System;
 public partial class GameManager : Node
 {
     // this is an example of the use of command pattern to use it in the GameManager
-    private static GameManager Instance;
+    public static GameManager Instance;
 
     public GameState State;
 
@@ -13,8 +13,11 @@ public partial class GameManager : Node
 
     //public static event Action<GameState>OnGameStateChanged; //this does not seem to work in godot but it works in unity -> [Signal] is the equivalent is seems
 
-[Signal]
-public delegate void SendingStateSignalEventHandler();
+
+    //this sends a signal everywhere to all scripts and thells them that there that if something happens in this function (in my case "UpdateGameState2") then it will send a signal so that it triggers an action everywhere where the scripts are listening
+    //to this signal -> UpdateGameState2"
+    [Signal]
+    public delegate void UpdateGameState2EventHandler(int fem);
 
 
 // the awake function runs when an enabled script instance is being loaded, the difference between this and ready is that ready will run once when class is called. i think...
@@ -22,19 +25,34 @@ public delegate void SendingStateSignalEventHandler();
     {
         Instance = this;
     }
+
     public override void _Ready()
     {
+       // This line below will add the signals function to the function "Test", if something happens to the function "Test" then it will trigger a signal to all other functions that are connected to "Test" in this case we have connected it to MenuManagers function
+       // "Test2" and there it will be triggerd, it will also send the information in "int fem" to all the functions everywhere!
+        UpdateGameState2 += Test;
         UpdateGameState(GameState.Menu);
+        //EmitSignal(nameof(UpdateGameState2), 5); // This Emit signal will send a signal from "UpdateGameState2" that we have to name as a string therefor the nameof(IpdateGameState2) to all functions that are listening and send the value - > 5 everywhere!
+        // in other words it gives int fem = 5;
+    }
+
+    static void Test(int fem)
+    {
+        GD.Print(fem);
     }
 
     public void UpdateGameState(GameState newState)
     {
+
+        //EmitSignal(nameof(UpdateGameState2), 5);
+
         State = newState;
         switch (newState)
         {
             case GameState.Menu:
                 HandleMenuSelect();
-                //GD.Print("hi");
+                EmitSignal(nameof(UpdateGameState2), 5);
+                GD.Print("hi");
                 break;
             case GameState.PlayerManager:
                 GD.Print("hi there, seems fine");
@@ -49,12 +67,10 @@ public delegate void SendingStateSignalEventHandler();
 
     }
 
-
     private void HandleLevelSelect()
     {
         throw new NotImplementedException();
     }
-
 
     private void HandlePlayerSelect()
     {
@@ -64,7 +80,9 @@ public delegate void SendingStateSignalEventHandler();
 
     private void HandleMenuSelect()
     {
+        //UpdateGameState(GameState.PlayerManager);
         //throw new NotImplementedException();
+       // EmitSignal(nameof(UpdateGameState2), 5);
     }
 
 }
--- Scripts/MenuManager.cs
+++ Scripts/MenuManager.cs
@@ -1,6 +1,8 @@
 using Godot;
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
+using static System.Net.Mime.MediaTypeNames;
 
 public partial class MenuManager : Control
 {
@@ -9,26 +11,56 @@ public partial class MenuManager : Control
     [Export]
     private Button _pressButton;
 
+    public static MenuManager Instance;
 
+    [Export]
     private GameManager _GetSignalFromGameManager;
+
+    void Awake ()
+    {
+        Instance = this;
+    }
 
     public override void _Ready()
     {
         //Getting signal from GameManager.
         _GetSignalFromGameManager = GetNode<GameManager>("/root/Managers/GameManager");
+        _GetSignalFromGameManager.UpdateGameState2 += Test2;
     }
 
-    void OnButtonPressed()
+
+    public void Test2(int fe)
+    {
+        GD.Print(fe+1);
+
+    }
+
+    void OnButtonPressed() // when pressing Start the State should change to PlayerState but now it would be nice with whichever tho...
     {
         GD.Print("Hello " + _GetSignalFromGameManager.State);
-        _GetSignalFromGameManager.State++;
+        _GetSignalFromGameManager.State++; // changes the state to next in enum in the GameManager script which is PlayerState
         GD.Print("Hello again " + _GetSignalFromGameManager.State);
-        
+
+        _GetSignalFromGameManager.EmitSignal(nameof(_GetSignalFromGameManager.UpdateGameState2), 5);
+
+        if (GameState.Menu != _GetSignalFromGameManager.State)
+        {
+            this.Hide();
+
+        }
         //not getting this to work
-        EmitSignal(nameof(_GetSignalFromGameManager.State));
+        //EmitSignal(MySignal);
+
+        //GameManager.Instance.UpdateGameState(GameState.PlayerManager); // why isnt this one working?
 
 
     }
 
+    void OnButtonPressed2() // this button is connected to "Quit"
+    {
+        //When pressed the game should quit.
+        GetTree().Quit();
+    }
 
+    
 }
